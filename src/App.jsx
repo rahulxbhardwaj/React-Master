45ee5a0c2211d7bfa657abdfef6882dbd6928176
rahulxbhardwaj <147973917+rahulxbhardwaj@users.noreply.github.com>
2024-03-16 05:43:56 +0000

    tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
+import { useState } from 'react'
 import './App.css'
+import './index.css'
 import Chai from './Chai.jsx'
 
 export default function App() {
+  
+
+
   return (
-    <main>
-      <Chai />
-      <h2>Hello</h2>
-    </main>
+  <>
+    <h1 className = 'bg-green-400'>Tailwind Test</h1>
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+  </>
   )
 }
+ 
